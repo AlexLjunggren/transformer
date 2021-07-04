c658cf2719cb86d60f408a84550e632ac56c120f
aljunggren <aljunggren@allegient.com>
2021-07-04 10:11:59 -0400

    Array - List cleanup

--- src/main/java/com/ljunggren/transformer/Transformer.java
+++ src/main/java/com/ljunggren/transformer/Transformer.java
@@ -43,8 +43,7 @@ public class Transformer {
     private List<Field> findObjectFields(Object object) {
         if (object != null) {
             Class<?> clazz = object.getClass();
-            Field[] fields = FieldUtils.getAllFields(clazz);
-            return Arrays.asList(fields);
+            return FieldUtils.getAllFieldsList(clazz);
         }
         return new ArrayList<Field>();
     }
